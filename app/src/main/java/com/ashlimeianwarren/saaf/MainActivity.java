daf92df8cc3f0fd312d0f153f7785d5e1cdf2fc2
Ian Weeks <ianweeks2004@yahoo.com>
2015-02-25 15:37:20 +0000

    Merge fix

--- app/src/main/java/com/ashlimeianwarren/saaf/MainActivity.java
+++ app/src/main/java/com/ashlimeianwarren/saaf/MainActivity.java
@@ -1,9 +1,11 @@
 package com.ashlimeianwarren.saaf;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 import com.ashlimeianwarren.saaf.Implementation.DbCon;
 
@@ -45,5 +47,11 @@ public class MainActivity extends ActionBarActivity
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void launchAsh(View view)
+    {
+        Intent myIntent = new Intent(this, AshTestActivity.class);
+        startActivity(myIntent);
     }
 }
