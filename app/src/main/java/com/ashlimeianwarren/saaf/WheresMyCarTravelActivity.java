fa977e6e14e26f0d1c6b339a482a2d6d944608a0
Ian <ianweeks2004@yahoo.com>
2015-04-09 12:10:44 +0100

    GPS improvements

--- app/src/main/java/com/ashlimeianwarren/saaf/WheresMyCarTravelActivity.java
+++ app/src/main/java/com/ashlimeianwarren/saaf/WheresMyCarTravelActivity.java
@@ -17,11 +17,13 @@ import android.widget.TextView;
 import android.hardware.SensorEventListener;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
+import android.widget.Toast;
 
 import com.ashlimeianwarren.saaf.Implementation.PositionManager;
 
 
-public class WheresMyCarTravelActivity extends ActionBarActivity implements SensorEventListener {
+public class WheresMyCarTravelActivity extends ActionBarActivity implements SensorEventListener
+{
     private ImageView pointerTwo;
     private SensorManager sensorManager;
     private Sensor accel;
@@ -46,7 +48,8 @@ public class WheresMyCarTravelActivity extends ActionBarActivity implements Sens
 
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_wheres_my_car_travel);
 
@@ -59,10 +62,13 @@ public class WheresMyCarTravelActivity extends ActionBarActivity implements Sens
 
         Bundle extras = getIntent().getExtras();
         String name = "";
-        if (extras != null) {
+        if (extras != null)
+        {
             oldLocation = (Location) extras.get("currentLocation");
             name = extras.getString("locationName");
-        } else {
+        }
+        else
+        {
             finish();
         }
 
@@ -78,14 +84,16 @@ public class WheresMyCarTravelActivity extends ActionBarActivity implements Sens
 
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_wheres_my_car_test, menu);
         return true;
     }
 
     @Override
-    protected void onResume() {
+    protected void onResume()
+    {
         super.onResume();
 
         pm.start();
@@ -94,7 +102,8 @@ public class WheresMyCarTravelActivity extends ActionBarActivity implements Sens
     }
 
     @Override
-    protected void onPause() {
+    protected void onPause()
+    {
         super.onPause();
 
         pm.close();
@@ -103,49 +112,66 @@ public class WheresMyCarTravelActivity extends ActionBarActivity implements Sens
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_settings)
+        {
             return true;
         }
 
         return super.onOptionsItemSelected(item);
     }
 
-    public void startNavClicked(View view) {
-
-    }
-
     @Override
-    public void onSensorChanged(SensorEvent event) {
-        if (event.sensor == accel) {
+    public void onSensorChanged(SensorEvent event)
+    {
+        if (event.sensor == accel)
+        {
             System.arraycopy(event.values, 0, lastAccelerometer, 0, event.values.length);
             adjLastAccelerometer = lowPass(event.values.clone(), adjLastAccelerometer);
             lastAccelerometerSet = true;
-        } else if (event.sensor == magne) {
+        }
+        else if (event.sensor == magne)
+        {
             System.arraycopy(event.values, 0, lastMagnetometer, 0, event.values.length);
             adjLastMagnetometer = lowPass(event.values.clone(), adjLastMagnetometer);
             lastMagnetometerSet = true;
         }
-        if (lastAccelerometerSet && lastMagnetometerSet) {
+        if (lastAccelerometerSet && lastMagnetometerSet)
+        {
             SensorManager.getRotationMatrix(mR, null, adjLastAccelerometer, adjLastMagnetometer);
             SensorManager.getOrientation(mR, orientation);
             azimuthInRadians = orientation[0];
             azimuthInDegress = (float) (Math.toDegrees(azimuthInRadians) + 360) % 360;
             currentDegree = (int) -azimuthInDegress;
         }
-        if (pm.getCurrentPosition() != null) {
+        if (pm.getCurrentPosition() != null)
+        {
             currentLocation = pm.getCurrentPosition();
             double dist = currentLocation.distanceTo(oldLocation);
 
+            if(dist < 5)
+            {
+                Toast.makeText(getApplicationContext(),
+                        "You have reached your destination.", Toast.LENGTH_LONG).show();
+                finish();
+            }
+
             distanceDisplay.setText(String.valueOf(dist));
 
-            RotateAnimation ra = new RotateAnimation(currentDegree + (float) pm.getBearingToLocation(oldLocation), (-azimuthInDegress + (float) pm.getBearingToLocation(oldLocation)), Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
+            RotateAnimation ra = new RotateAnimation(
+                    currentDegree + (float) pm.getBearingToLocation(oldLocation),
+                    (-azimuthInDegress + (float) pm.getBearingToLocation(oldLocation)),
+                    Animation.RELATIVE_TO_SELF,
+                    0.5f,
+                    Animation.RELATIVE_TO_SELF,
+                    0.5f);
             ra.setDuration(2000);
 
             ra.setFillAfter(true);
@@ -155,31 +181,43 @@ public class WheresMyCarTravelActivity extends ActionBarActivity implements Sens
     }
 
     @Override
-    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+    public void onAccuracyChanged(Sensor sensor, int accuracy)
+    {
         // Not used but required for the implementation.
     }
 
-    private float[] lowPass(float[] input, float[] output) {
-        if (output == null) {
+    private float[] lowPass(float[] input, float[] output)
+    {
+        if (output == null)
+        {
             return input;
         }
-        for (int i = 0; i < input.length; i++) {
+        for (int i = 0; i < input.length; i++)
+        {
             output[i] = output[i] + ALPHA * (input[i] - output[i]);
         }
         return output;
     }
 
-    private void buildAlertMessageArrived() {
+    private void buildAlertMessageArrived()
+    {
         final AlertDialog.Builder builder = new AlertDialog.Builder(this);
         builder.setMessage("You have arrived at you save location.")
                 .setCancelable(false)
-                .setNegativeButton("Yes", new DialogInterface.OnClickListener() {
-                    public void onClick(@SuppressWarnings("unused") final DialogInterface dialog, @SuppressWarnings("unused") final int id) {
-                        startActivity(new Intent(android.provider.Settings.ACTION_LOCATION_SOURCE_SETTINGS));
+                .setNegativeButton("Yes", new DialogInterface.OnClickListener()
+                {
+                    public void onClick(@SuppressWarnings("unused") final DialogInterface dialog,
+                                        @SuppressWarnings("unused") final int id)
+                    {
+                        startActivity(new Intent(
+                                android.provider.Settings.ACTION_LOCATION_SOURCE_SETTINGS));
                     }
                 })
-                .setPositiveButton("No", new DialogInterface.OnClickListener() {
-                    public void onClick(final DialogInterface dialog, @SuppressWarnings("unused") final int id) {
+                .setPositiveButton("No", new DialogInterface.OnClickListener()
+                {
+                    public void onClick(final DialogInterface dialog,
+                                        @SuppressWarnings("unused") final int id)
+                    {
                         finish();
                     }
                 });
